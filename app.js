050c9b7f335318f4d9accb00ecd89bf91df21124
MuhireIghor <muhireighor123@gmail.com>
2022-11-12 13:49:12 +0200

    random user generation

--- app.js
+++ app.js
@@ -9,12 +9,15 @@ const app = Vue.createApp({
         }
     },
     methods:{
-        getUser(){
-            this.firstName="Muhire",
-            this.lastName="Ighor",
-            this.email="muhireighor123@gmail.com",
-            this.gender = "male",
-            this.picture="https://randomuser.me/api/portraits/men/11.jpg"
+       async getUser(){
+            const res = await fetch("https://randomuser.me/api");
+            const{results} = await res.json();
+            this.firstName=results[0].name.first
+            this.lastName=results[0].name.last
+            this.email=results[0].email
+            this.gender = results[0].gender
+            this.picture=results[0].picture.large
+      
         }
     }
 })
