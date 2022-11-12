d7804ddb9b485f24816875ec88ee0408411438ff
MuhireIghor <muhireighor123@gmail.com>
2022-11-12 13:41:06 +0200

    method modification

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ const app = Vue.createApp({
             gender:"male",
             picture:"https://randomuser.me/api/portraits/men/10.jpg"
         }
+    },
+    methods:{
+        getUser(){
+            this.firstName="Muhire",
+            this.lastName="Ighor",
+            this.email="muhireighor123@gmail.com",
+            this.gender = "male",
+            this.picture="https://randomuser.me/api/portraits/men/11.jpg"
+        }
     }
 })
 app.mount('#app')
